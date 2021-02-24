f4076d8e7720ae3535ccbd1d74a27cb3c858725f
이관희 <akrnrnak@gmail.com>
2021-02-24 12:49:00 +0900

    detach checkPrime.js from updatelast.js

---
+++ controllers/util/checkPrime.js
@@ -0,0 +1,27 @@
+module.exports = function (num) {
+    let result = ""
+
+    if(num != parseInt(num) || num < 1)
+        result = "Enter a positive integer."
+    else if(num == 1)
+        result = "False"
+    else if(num <= 3)
+        result = "True"
+    else {
+        let check = true
+        
+        const squareRoot = parseInt(Math.sqrt(num))
+        for(let i = 2; i <= squareRoot; i++) {
+            if((num % i) == 0) {
+                check = false
+                break
+            }
+        }
+        if(check)
+            result = "True"
+        else
+            result = "False"
+    }
+
+    return result
+}
