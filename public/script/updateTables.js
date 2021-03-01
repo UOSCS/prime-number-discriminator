da07f1f370f2dbf24dd3869e6fd0b32acb54ca19
이관희 <akrnrnak@gmail.com>
2021-03-01 18:27:05 +0900

    limit input range & error fix

--- public/script/updateTables.js
+++ public/script/updateTables.js
@@ -3,28 +3,34 @@ const resultBtn = document.getElementById("btn")
 resultBtn
 .addEventListener("click", () => {
     const text = document.getElementById("input").value
-    // POST
-    fetch("/result", {
-        method: "POST",
-        body: JSON.stringify({
-            text: text,
-        }),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    .then(async (response) => {
-        const element = await response.json()
+    
+    document.getElementById("input").value = ""
+    document.getElementById("input").focus()
 
-        document.getElementById("input").value = ""
-
-        if(!element.last) {
-            throw new Error("Enter a positive integer")
-        }
-        else {
-            updateRank(element.rank)
-            updateLast(element.last)
-        }
-    })
-    .catch (error => alert(error))
+    if(text > Number.MAX_SAFE_INTEGER) {
+        alert(`Error: A safe range of integer: 1 ~ ${Number.MAX_SAFE_INTEGER}`)
+    } else {
+        // POST
+        fetch("/result", {
+            method: "POST",
+            body: JSON.stringify({
+                text: text,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then(async (response) => {
+            const element = await response.json()
+    
+            if(!element.last) {
+                throw new Error("Enter a positive integer")
+            }
+            else {
+                updateRank(element.rank)
+                updateLast(element.last)
+            }
+        })
+        .catch (error => alert(error))
+    }
 })
